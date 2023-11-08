057a9657e347a4951d9b90feff74db56df5d7c39
AmenyMlh <mellahameny@gmail.com>
2023-11-08 20:12:44 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const express = require("express");
+
+
+const app = express();
+
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://localhost:27017/BD_DS")
+  .then(() => console.log("Connexion a MongoDb réussie !!"))
+  .catch(() => console.log("Connexion a MongoDB échouée !!!!!!"));
+
+
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-with, Content,Accept,Content-Type,Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT,DELETE,PATCH,OPTIONS"
+  );
+  next();
+});
+
+
+
+
+
+module.exports = app;
